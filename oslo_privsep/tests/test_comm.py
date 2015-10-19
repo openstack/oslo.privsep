025dd2476fbb3fd0b3aefe338e709bdc3de2ac31
Angus Lees <gus@inodes.org>
2015-10-19 18:22:16 +1100

    Initial basic privsep functionality
    Supports starting privileged process via fork or sudo, and a thread-safe
    client and server communication mechanism.
    
    Coming in later changes:
     - Extend json encoding to deal with non-unicode and more varied dict
       keys (see test_comm.py).
     - eventlet test case.
     - Linux capabilities.
    
    Change-Id: If6456631c51d4f2a1c95805ab9d6962b04f172bc
    Implements: blueprint privsep

---
+++ oslo_privsep/tests/test_comm.py
@@ -0,0 +1,107 @@
+# Copyright 2015 Rackspace Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import six
+
+from oslotest import base
+
+from oslo_privsep import comm
+
+
+class BufSock(object):
+    def __init__(self):
+        self.readpos = 0
+        self.buf = six.BytesIO()
+
+    def recv(self, bufsize):
+        if self.buf.closed:
+            return b''
+        self.buf.seek(self.readpos, 0)
+        data = self.buf.read(bufsize)
+        self.readpos += len(data)
+        return data
+
+    def sendall(self, data):
+        self.buf.seek(0, 2)
+        self.buf.write(data)
+
+    def shutdown(self, _flag):
+        self.buf.close()
+
+
+class TestSerialization(base.BaseTestCase):
+    def setUp(self):
+        super(TestSerialization, self).setUp()
+
+        sock = BufSock()
+
+        self.input = comm.Serializer(sock)
+        self.output = iter(comm.Deserializer(sock))
+
+    def send(self, data):
+        self.input.send(data)
+        return next(self.output)
+
+    def assertSendable(self, value):
+        self.assertEqual(value, self.send(value))
+
+    def test_none(self):
+        self.assertSendable(None)
+
+    def test_bool(self):
+        self.assertSendable(True)
+        self.assertSendable(False)
+
+    def test_int(self):
+        self.assertSendable(42)
+        self.assertSendable(-84)
+
+    def test_bytes(self):
+        # TODO(gus): json needs help to support non-unicode strings
+        # data = b'\x00\x01\x02\xfd\xfe\xff'
+        # self.assertSendable(data)
+        pass
+
+    def test_unicode(self):
+        data = u'\u4e09\u9df9'
+        self.assertSendable(data)
+
+    def test_tuple(self):
+        # NB! currently tuples get converted to lists by serialization.
+        self.assertEqual([1, 'foo'], self.send((1, 'foo')))
+
+    def test_list(self):
+        self.assertSendable([1, 'foo'])
+
+    def test_dict(self):
+        self.assertSendable(
+            {
+                'a': 'b',
+                # TODO(gus): json needs help to support non-string keys
+                # 1: 2,
+                # None: None,
+                # (1, 2): (3, 4),
+            }
+        )
+
+    def test_badobj(self):
+        class UnknownClass(object):
+            pass
+
+        obj = UnknownClass()
+        self.assertRaises(TypeError, self.send, obj)
+
+    def test_eof(self):
+        self.input.close()
+        self.assertRaises(StopIteration, next, self.output)
---
+++ oslo_privsep/tests/test_priv_context.py
@@ -0,0 +1,115 @@
+# Copyright 2015 Rackspace Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import os
+import pipes
+import sys
+
+from oslo_log import log as logging
+
+from oslo_privsep import daemon
+from oslo_privsep import priv_context
+from oslo_privsep.tests import testctx
+
+LOG = logging.getLogger(__name__)
+
+
+@testctx.context.entrypoint
+def priv_getpid():
+    return os.getpid()
+
+
+@testctx.context.entrypoint
+def add1(arg):
+    return arg + 1
+
+
+class CustomError(Exception):
+    def __init__(self, code, msg):
+        super(CustomError, self).__init__(code, msg)
+        self.code = code
+        self.msg = msg
+
+    def __str__(self):
+        return 'Code %s: %s' % (self.code, self.msg)
+
+
+@testctx.context.entrypoint
+def fail(custom=False):
+    if custom:
+        raise CustomError(42, 'omg!')
+    else:
+        raise RuntimeError("I can't let you do that Dave")
+
+
+class TestSeparation(testctx.TestContextTestCase):
+    def test_getpid(self):
+        # Verify that priv_getpid() was executed in another process.
+        priv_pid = priv_getpid()
+        self.assertNotMyPid(priv_pid)
+
+    def test_client_mode(self):
+        self.assertNotMyPid(priv_getpid())
+
+        self.addCleanup(testctx.context.set_client_mode, True)
+
+        testctx.context.set_client_mode(False)
+        # priv_getpid() should now run locally (and return our pid)
+        self.assertEqual(os.getpid(), priv_getpid())
+
+        testctx.context.set_client_mode(True)
+        # priv_getpid() should now run remotely again
+        self.assertNotMyPid(priv_getpid())
+
+
+class RootwrapTest(testctx.TestContextTestCase):
+    def setUp(self):
+        super(RootwrapTest, self).setUp()
+        testctx.context.stop()
+
+        # Generate a command that will run daemon.helper_main without
+        # requiring it to be properly installed.
+        cmd = [
+            'env',
+            'PYTHON_PATH=%s' % os.path.pathsep.join(sys.path),
+            sys.executable, daemon.__file__,
+        ]
+        if LOG.isEnabledFor(logging.DEBUG):
+            cmd.append('--debug')
+
+        self.privsep_conf.set_override(
+            'helper_command', ' '.join(map(pipes.quote, cmd)),
+            group=testctx.context.cfg_section)
+
+        testctx.context.start(method=priv_context.Method.ROOTWRAP)
+
+    def test_getpid(self):
+        # Verify that priv_getpid() was executed in another process.
+        priv_pid = priv_getpid()
+        self.assertNotMyPid(priv_pid)
+
+
+class TestSerialization(testctx.TestContextTestCase):
+    def test_basic_functionality(self):
+        self.assertEqual(43, add1(42))
+
+    def test_raises_standard(self):
+        self.assertRaisesRegexp(
+            RuntimeError, "I can't let you do that Dave", fail)
+
+    def test_raises_custom(self):
+        exc = self.assertRaises(CustomError, fail, custom=True)
+        self.assertEqual(exc.code, 42)
+        self.assertEqual(exc.msg, 'omg!')
